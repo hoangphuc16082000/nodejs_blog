a5bd847cdf3ec69ea92d0decf62a272c6c6c406a
hoangphuc16082000 <hoangphuc16082000@gmail.com>
2024-10-24 15:20:32 +0700

    delete course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-var slug = require('mongoose-slug-generator');
 
-mongoose.plugin(slug);
+// lib mongoose-slug-generator đang bị lỗi version nên chuyển qua dùng slugify
+const slugify = require('slugify');
 
 const Schema = mongoose.Schema;
 
@@ -11,12 +11,17 @@ const Course = new Schema({
     image: { type: String },
     videoId: { type: String, required: true },
     level: { type: String, maxLength: 255 },
-    slug: { type: String, slug: 'name', unique: true }
+    slug: { type: String, unique: true }
 }, { 
     timestamps: true 
 });
 
 // timestamps : tự tạo createAt & updateAt
 // unique : khi tạo slug thì không cho phép tạo 2 slag giống nhau
+
+Course.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true, strict: true });
+    next();
+});
 
 module.exports = mongoose.model('Course', Course);
